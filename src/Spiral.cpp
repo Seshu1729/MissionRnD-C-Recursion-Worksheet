bac290343991c6834e9eecc4bd58df6ca979e069
Seshu1729 <Seshu1729@missionrnd.com>
2016-03-25 15:34:07 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -34,7 +34,54 @@ Note : Check the function Parameters ,Its a double pointer .
 #include "stdafx.h"
 #include<stdlib.h>
 
+int** allocate_memory(int rows, int columns)
+{
+	int** return_array;
+	int counter;
+	return_array = (int **)malloc(sizeof(int*)*(rows));
+	for (counter = 0; counter<rows; counter++)
+		*(return_array + counter) = (int *)malloc(sizeof(int)*(columns));
+	return return_array;
+}
+
+int **rotate_matrix_by_ninty_degrees_anticlockwise(int rows, int columns, int **input_array)
+{
+	int **convected_matrix, i, j;
+	convected_matrix = allocate_memory(columns, rows);
+	for (i = 0; i<columns; i++)
+	{
+		for (j = 0; j<rows; j++)
+		{
+			convected_matrix[i][j] = input_array[j][columns - 1 - i];
+		}
+	}
+	return convected_matrix;
+}
+
+int *combine_two_arrays(int *array1, int *array2, int size1, int size2)
+{
+	int index, *combine_array;
+	combine_array = (int *)malloc(sizeof(int)*(size1 + size2));
+	for (index = 0; index<size1; index++)
+		combine_array[index] = array1[index];
+	for (; index<size1 + size2; index++)
+		combine_array[index] = array2[index - size1];
+	return combine_array;
+}
+
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	int  *result_from_next_function_call, **rotated_matrix;
+
+	//recursion terminate condition
+	if (rows == 1)
+		return *input_array;
+
+	//basic condition
+	if (rows <= 0 || columns <= 0 || input_array == NULL)
+		return NULL;
+
+	rotated_matrix = rotate_matrix_by_ninty_degrees_anticlockwise(rows - 1, columns, input_array + 1);
+	result_from_next_function_call = spiral(columns, rows - 1, rotated_matrix);
+	return combine_two_arrays(*input_array, result_from_next_function_call, columns, (rows - 1)*columns);
 }
